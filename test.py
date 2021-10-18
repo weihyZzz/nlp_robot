55f4c064faa2750e8c676775f924049cff1e8b6f
weihyZzz <971973918@qq.com>
2021-10-18 19:00:49 +0800

    张三对test的第一次修改

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-
+﻿
 #导入库
 from model.model import *
 from model.pre_process import *
@@ -9,6 +9,6 @@ chatbot = ChatBot('model.pkl')
 
 #针对问题1
 chatbot.predictByGreedySearch("你好啊")
-
+#李四对test进行第一次修改
 #针对问题2
 chatbot.predictByBeamSearch("什么是人工智能", isRandomChoose=True, beamWidth=10)
